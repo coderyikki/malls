a82c9bd07c2db08a22754f792787f6a520722a1a
yikki <1259544280@qq.com>
2021-03-03 23:15:04 +0800

    初始化

--- src/network/home.js
+++ src/network/home.js
@@ -5,3 +5,13 @@ export function getHomeMultidata(){
         url: '/home/data?type=sell&page=1'
     })
 }
+
+export function getHomeGoods(type, page){
+    return request({
+        url:'/home/data',
+        params:{
+            type,
+            page
+        }
+    })
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,23 +8,43 @@
 
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
-import { getHomeMultidata } from "../../network/home";
+import { getHomeMultidata, getHomeGoods } from "../../network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar,
   },
-  data(){
+  data() {
     return {
-      banners:[]
-    }
+      banners: [],
+      goods: {
+        'pop': { page: 0, list: [] },
+        'new': { page: 0, list: [] },
+        'sell': { page: 0, list: [] },
+      },
+    };
   },
+
   created() {
-    getHomeMultidata().then(res=> {
-      console.log(res);
-      this.banner = res.data.banner;
-    })
+    this.getHomeMultidata();
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banners;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      });
+    },
   },
 };
 </script>
